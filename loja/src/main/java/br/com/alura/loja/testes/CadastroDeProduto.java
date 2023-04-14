8173a266a561efabd8b315ff155453412f07a702
Hebert <hebertLandim007@outlook.com>
2023-04-14 17:32:32 -0300

    States do not exclude the entity

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -22,6 +22,8 @@ public class CadastroDeProduto {
         celulares = em.merge(celulares);
         celulares.setNome("1234");
         em.flush();
+        em.remove(celulares);
+        em.flush();
     }
 
 }
