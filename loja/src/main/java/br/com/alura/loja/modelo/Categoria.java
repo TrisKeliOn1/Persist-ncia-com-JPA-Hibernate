4204ac235728a1b74b35cbc12b7a07be0af4a675
Hebert <hebertLandim007@outlook.com>
2023-04-16 10:02:46 -0300

    queries with JPQL

--- loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -12,20 +12,19 @@ public class Categoria {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    
-    public Categoria() {		
+	private Long id;
+	private String nome;
+	
+	public Categoria() {
 	}
-    
+	
 	public Categoria(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
