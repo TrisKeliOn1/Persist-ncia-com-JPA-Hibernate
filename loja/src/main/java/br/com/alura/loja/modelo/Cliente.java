99a3a9b889ce663be580f5532b866091a66092f0
Hebert <hebertLandim007@outlook.com>
2023-04-17 11:20:12 -0300

    bidirectional relationship test

--- loja/src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Clientes")
+@Table(name = "clientes")
 public class Cliente {
 
 	@Id
--- loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
+++ loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
@@ -2,32 +2,33 @@ package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "itens_pedido")
 public class ItemPedido {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(name = "preco_unitario")
 	private BigDecimal precoUnitario;
-	
+
 	private int quantidade;
-	
+
 	@ManyToOne
 	private Pedido pedido;
-	
-	@ManyToMany
+
+	@ManyToOne
 	private Produto produto;
-	
+
 	public ItemPedido() {
 	}
 
@@ -77,7 +78,5 @@ public class ItemPedido {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
-	
 
 }
