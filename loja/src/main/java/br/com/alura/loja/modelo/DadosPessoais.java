6489862a135c8cf35872fdff2205dcc78e0bfcf2
Hebert <hebertLandim007@outlook.com>
2023-04-19 10:38:38 -0300

    Simplifying entities with Embeddable

---
+++ loja/src/main/java/br/com/alura/loja/modelo/DadosPessoais.java
@@ -0,0 +1,31 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DadosPessoais {
+	
+	private String nome;
+	private String cpf;
+	
+	public DadosPessoais() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public DadosPessoais(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+}
